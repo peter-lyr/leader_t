6af6f8dd194ba745e0c8931b1b4fc70806b4dd1b
peter-lyr <llydrp.ldp@gmail.com>
2024-09-17 00:08:29 +0800

    ^<leader^>tu F.nvimtree_cd_sel(F.get_cur_proj_dirs())

--- plugin/leader_t.lua
+++ plugin/leader_t.lua
@@ -9,6 +9,7 @@ require 'which-key'.register {
   ['<leader>ts'] = { function() vim.cmd 'StartupTime' end, 'StartupTime', mode = { 'n', 'v', }, },
   ['<leader>tc'] = { function() F.nvimtree_cd(F.get_cur_file()) end, 'nvimtree_cd %:h', mode = { 'n', 'v', }, },
   ['<leader>tw'] = { function() F.nvimtree_cd(F.get_cwd()) end, 'nvimtree_cd cwd', mode = { 'n', 'v', }, },
+  ['<leader>tu'] = { function() F.nvimtree_cd_sel(F.get_cur_proj_dirs()) end, 'nvimtree_cd sel', mode = { 'n', 'v', }, },
   ['<leader>to'] = { function() vim.cmd 'NvimTreeOpen' end, 'NvimTreeOpen', mode = { 'n', 'v', }, },
   ['<leader>tq'] = { function() vim.cmd 'NvimTreeClose' end, 'NvimTreeClose', mode = { 'n', 'v', }, },
   ['<leader>tt'] = { function() test() end, 'test', mode = { 'n', 'v', }, },
