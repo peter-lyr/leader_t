d3ceae1dc2babd6ec59b305a8fb3576d450a9877
peter-lyr <llydrp.ldp@gmail.com>
2024-09-16 23:45:35 +0800

    ^<leader^>to NvimTreeOpen

--- plugin/leader_t.lua
+++ plugin/leader_t.lua
@@ -1,4 +1,10 @@
+local F = require 'f'
+
 require 'which-key'.register {
   ['<leader>t'] = { name = 'test', },
   ['<leader>ts'] = { function() vim.cmd 'StartupTime' end, 'StartupTime', mode = { 'n', 'v', }, },
+  ['<leader>tc'] = { function() F.nvimtree_cd(F.get_cur_file()) end, 'nvimtree_cd %:h', mode = { 'n', 'v', }, },
+  ['<leader>tw'] = { function() F.nvimtree_cd(F.get_cwd()) end, 'nvimtree_cd cwd', mode = { 'n', 'v', }, },
+  ['<leader>to'] = { function() vim.cmd 'NvimTreeOpen' end, 'NvimTreeOpen', mode = { 'n', 'v', }, },
+  ['<leader>tq'] = { function() vim.cmd 'NvimTreeClose' end, 'NvimTreeClose', mode = { 'n', 'v', }, },
 }
