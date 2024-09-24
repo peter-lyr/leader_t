2e82157f6070712ee6c6cc3d503524ef2e93e522
peter-lyr <llydrp.ldp@gmail.com>
2024-09-24 21:52:06 +0800

    F.source_cur()

--- plugin/leader_t.lua
+++ plugin/leader_t.lua
@@ -28,7 +28,7 @@ end
 
 require 'which-key'.register {
   ['<leader>t'] = { name = 'test', },
-  ['<leader>ts'] = { function() vim.cmd 'StartupTime' end, 'StartupTime', mode = { 'n', 'v', }, },
+  ['<leader>tS'] = { function() vim.cmd 'StartupTime' end, 'StartupTime', mode = { 'n', 'v', }, },
   ['<leader>tc'] = { function() F.nvimtree_cd(F.get_cur_file()) end, 'nvimtree_cd %:h', mode = { 'n', 'v', }, },
   ['<leader>tw'] = { function() F.nvimtree_cd(F.get_cwd()) end, 'nvimtree_cd cwd', mode = { 'n', 'v', }, },
   ['<leader>tu'] = { function() F.nvimtree_cd_sel(F.get_cur_proj_dirs()) end, 'nvimtree_cd sel', mode = { 'n', 'v', }, },
@@ -40,5 +40,5 @@ require 'which-key'.register {
   ['<leader>tt'] = { function() test() end, 'test', mode = { 'n', 'v', }, },
   ['<leader>tb'] = { name = 'output buffer', },
   ['<leader>tbn'] = { function() F.notifications_buffer() end, 'notifications_buffer', mode = { 'n', 'v', }, },
-  ['<leader>tT'] = { function() F.source_cur() end, 'source_cur', mode = { 'n', 'v', }, },
+  ['<leader>ts'] = { function() F.source_cur() end, 'source_cur', mode = { 'n', 'v', }, },
 }
