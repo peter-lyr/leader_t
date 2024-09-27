80d5ab4c82253c802f94290d7a671784badf570e
peter-lyr <llydrp.ldp@gmail.com>
2024-09-27 23:15:58 +0800

    <leader>tnq

--- plugin/leader_t.lua
+++ plugin/leader_t.lua
@@ -28,8 +28,9 @@ end
 
 require 'which-key'.register {
   ['<leader>t'] = { name = 'test', },
-  ['<leader>tQ'] = { function() vim.cmd 'qa!' end, 'qa!', mode = { 'n', 'v', }, },
+
   ['<leader>tS'] = { function() vim.cmd 'StartupTime' end, 'StartupTime', mode = { 'n', 'v', }, },
+
   ['<leader>tc'] = { function() F.nvimtree_cd(F.get_cur_file()) end, 'nvimtree_cd %:h', mode = { 'n', 'v', }, },
   ['<leader>tw'] = { function() F.nvimtree_cd(F.get_cwd()) end, 'nvimtree_cd cwd', mode = { 'n', 'v', }, },
   ['<leader>tu'] = { function() F.nvimtree_cd_sel(F.get_cur_proj_dirs()) end, 'nvimtree_cd sel', mode = { 'n', 'v', }, },
@@ -38,11 +39,20 @@ require 'which-key'.register {
   ['<leader>tp'] = { function() vim.cmd 'NvimTreeFocus' end, 'NvimTreeFocus', mode = { 'n', 'v', }, },
   ['<leader>td'] = { function() F.nvimtree_findfile() end, 'nvimtree_findfile', mode = { 'n', 'v', }, },
   ['<leader>tf'] = { function() vim.cmd 'NvimTreeFindFile' end, 'nvimtree_findfile_just_update', mode = { 'n', 'v', }, },
-  ['<leader>tn'] = { name = 'nvim-qt', },
-  ['<leader>tnc'] = { function() F.cmd('silent !start nvim-qt.exe %s', F.get_cur_file()) end, 'silent! nvim-qt.exe cur', mode = { 'n', 'v', }, },
-  ['<leader>tnn'] = { function() vim.cmd 'silent !start nvim-qt.exe' end, 'silent! nvim-qt.exe', mode = { 'n', 'v', }, },
+
+  ['<leader>tn'] = { name = 'nvim-qt start', },
+  ['<leader>tnc'] = { function() F.start_nvim_qt(F.get_cur_file()) end, 'start_nvim_qt cur', mode = { 'n', 'v', }, },
+  ['<leader>tnn'] = { function() F.start_nvim_qt() end, 'start_nvim_qt', mode = { 'n', 'v', }, },
+  ['<leader>tnq'] = { function() vim.cmd 'qa!' end, 'qa!', mode = { 'n', 'v', }, },
+
+  ['<leader>tr'] = { name = 'nvim-qt restart', },
+  ['<leader>trc'] = { function() F.restart_nvim_qt(F.get_cur_file()) end, 'restart_nvim_qt cur', mode = { 'n', 'v', }, },
+  ['<leader>trn'] = { function() F.restart_nvim_qt() end, 'restart_nvim_qt', mode = { 'n', 'v', }, },
+
   ['<leader>tt'] = { function() test() end, 'test', mode = { 'n', 'v', }, },
+
   ['<leader>tb'] = { name = 'output buffer', },
   ['<leader>tbn'] = { function() F.notifications_buffer() end, 'notifications_buffer', mode = { 'n', 'v', }, },
+
   ['<leader>ts'] = { function() F.source_cur() end, 'source_cur', mode = { 'n', 'v', }, },
 }
