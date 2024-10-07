ab24f2baaea9e901308cb398cde28ced0348ae4b
peter-lyr <llydrp.ldp@gmail.com>
2024-10-07 11:23:57 +0800

    <leader>t<leader>u F.nvimtree_cd_sel_DIRS()

--- plugin/leader_t.lua
+++ plugin/leader_t.lua
@@ -47,12 +47,14 @@ end
 
 require 'which-key'.register {
   ['<leader>t'] = { name = 'test', },
+  ['<leader>t<leader>'] = { name = 'test.more', },
 
   ['<leader>tS'] = { function() vim.cmd 'StartupTime' end, 'StartupTime', mode = { 'n', 'v', }, },
 
   ['<leader>tc'] = { function() F.nvimtree_cd(F.get_cur_file()) end, 'nvimtree_cd %:h', mode = { 'n', 'v', }, },
   ['<leader>tw'] = { function() F.nvimtree_cd(F.get_cwd()) end, 'nvimtree_cd cwd', mode = { 'n', 'v', }, },
   ['<leader>tu'] = { function() F.nvimtree_cd_sel(F.get_cur_proj_dirs()) end, 'nvimtree_cd sel', mode = { 'n', 'v', }, },
+  ['<leader>t<leader>u'] = { function() F.nvimtree_cd_sel_DIRS() end, 'nvimtree_cd sel DIRS', mode = { 'n', 'v', }, },
   ['<leader>to'] = { function() vim.cmd 'NvimTreeOpen' end, 'NvimTreeOpen', mode = { 'n', 'v', }, },
   ['<leader>tq'] = { function() vim.cmd 'NvimTreeClose' end, 'NvimTreeClose', mode = { 'n', 'v', }, },
   ['<leader>tp'] = { function() vim.cmd 'NvimTreeFocus' end, 'NvimTreeFocus', mode = { 'n', 'v', }, },
