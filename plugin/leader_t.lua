b4b36bd93a737be855c3287139a49d37280f71b7
peter-lyr <llydrp.ldp@gmail.com>
2024-11-07 20:52:34 +0800

    <localleader>t<leader>b F.toggle_vim_g_winbar()

--- plugin/leader_t.lua
+++ plugin/leader_t.lua
@@ -97,6 +97,7 @@ require 'which-key'.register {
   ['<localleader>td'] = { function() F.toggle_local 'diff' end, 'toggle_diff', mode = { 'n', 'v', }, },
   ['<localleader>tw'] = { function() F.toggle_local 'wrap' end, 'toggle_wrap', mode = { 'n', 'v', }, },
   ['<localleader>tb'] = { function() F.toggle_winbar() end, 'toggle_winbar', mode = { 'n', 'v', }, },
+  ['<localleader>t<leader>b'] = { function() F.toggle_vim_g_winbar() end, 'toggle_vim_g_winbar', mode = { 'n', 'v', }, },
 }
 
 F.lazy_load 'todo_dial'
